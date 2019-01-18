4319a5b5f69d9238451066de097a9cb2e98cffdb
Philip van Allen <vanallen@artcenter.edu>
2019-01-18 12:32:59 -0800

    Added Otherwise condition, and made Contains condition allow for comma delimited strings as a kind of "OR"

--- unity/delft-toolkit/Assets/Scripts/delftToolkit/Nodes/StringCondition.cs
+++ unity/delft-toolkit/Assets/Scripts/delftToolkit/Nodes/StringCondition.cs
@@ -42,32 +42,35 @@ namespace DelftToolkit {
 			bool deactivate = false;
 			for (int i = 0; i < conditions.Length; i++) {
 				// We evaluate first because we want the lastState in the condition to update regardless of 'active'
-				if (conditions[i].Evaluate(value) && active) {
-					NodePort triggerPort = GetOutputPort("conditions " + i);
-					if (triggerPort.IsConnected) {
-						for (int k = 0; k < triggerPort.ConnectionCount; k++) {
-							StateNodeBase nextNode = triggerPort.GetConnection(k).node as StateNodeBase;
-							if (nextNode != null) nextNode.Enter();
+				if (conditions[i].Evaluate(value, deactivate)) {
+					// deactivate also used for determining otherwise condition
+					deactivate = true;
+					if (active) {
+						NodePort triggerPort = GetOutputPort("conditions " + i);
+						if (triggerPort.IsConnected) {
+							for (int k = 0; k < triggerPort.ConnectionCount; k++) {
+								StateNodeBase nextNode = triggerPort.GetConnection(k).node as StateNodeBase;
+								if (nextNode != null) nextNode.Enter();
+							}
 						}
 					}
-					deactivate = true;
 				}
 			}
 			if (deactivate) active = false;
 		}
 
 		[Serializable] public struct Condition {
-			public enum CompareType { StartsWith, EndsWith, Contains }
+			public enum CompareType { StartsWith, EndsWith, Contains, Otherwise }
 			[NodeEnum] public CompareType compareType;
 			public string strVal;
 			public bool inverse;
 			[NonSerialized] public bool lastState;
 
-			public bool Evaluate(string test) {
-				return lastState = EvaluateInternal(test) != inverse;
+			public bool Evaluate(string test, bool priorCompareSuccess) {
+				return lastState = EvaluateInternal(test, priorCompareSuccess) != inverse;
 			}
 
-			private bool EvaluateInternal(string test) {
+			private bool EvaluateInternal(string test, bool priorCompareSuccess ) {
 				test = test.ToLower();
 				switch (compareType) {
 					case CompareType.StartsWith:
@@ -75,7 +78,20 @@ namespace DelftToolkit {
 					case CompareType.EndsWith:
 						return test.EndsWith(strVal.ToLower());
 					case CompareType.Contains:
-						return test.Contains(strVal.ToLower());
+						if (strVal.Contains(",")) {
+							// check for multiple strings, any of which must be in target
+							bool comparison = false;
+							string[] theStrings = strVal.Split(',');
+							foreach (string item in theStrings) {
+								comparison = test.Contains(item.ToLower());
+								if (comparison)
+									break;
+							}
+							return comparison;
+						} else return test.Contains(strVal.ToLower());
+					case CompareType.Otherwise:
+						// if none of the prior conditions are true, this is true
+						return !priorCompareSuccess;
 					default:
 						return false;
 				}
