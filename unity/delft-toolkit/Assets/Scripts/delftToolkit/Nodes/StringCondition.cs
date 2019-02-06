a6781e1579d219770cabb6a6d13746d38ccf6d38
Philip van Allen <vanallen@artcenter.edu>
2019-02-05 17:36:18 -0800

    added "otherwise" condition, added comma delimited options for "contains"

--- unity/delft-toolkit/Assets/Scripts/delftToolkit/Nodes/StringCondition.cs
+++ unity/delft-toolkit/Assets/Scripts/delftToolkit/Nodes/StringCondition.cs
@@ -41,15 +41,19 @@ namespace DelftToolkit {
 		protected override void CheckConditions() {
 			// Store the active state because we want to evaluate all conditions, not just the first one
 			bool activeCache = active;
+			bool conditionMatch = false;
 			for (int i = 0; i < conditions.Length; i++) {
 				// We evaluate first because we want the lastState in the condition to update regardless of 'active'
-				if (conditions[i].Evaluate(value) && activeCache) {
-					NodePort triggerPort = GetOutputPort("conditions " + i);
-					if (triggerPort.IsConnected) {
-						for (int k = 0; k < triggerPort.ConnectionCount; k++) {
-							StateNodeBase nextNode = triggerPort.GetConnection(k).node as StateNodeBase;
-							active = false;
-							if (nextNode != null) nextNode.Enter();
+				if (conditions[i].Evaluate(value, conditionMatch) ) {
+					conditionMatch = true; // track condition matches for "otherwise" condition
+					if (activeCache) {
+						NodePort triggerPort = GetOutputPort("conditions " + i);
+						if (triggerPort.IsConnected) {
+							for (int k = 0; k < triggerPort.ConnectionCount; k++) {
+								StateNodeBase nextNode = triggerPort.GetConnection(k).node as StateNodeBase;
+								active = false;
+								if (nextNode != null) nextNode.Enter();
+							}
 						}
 					}
 				}
@@ -57,17 +61,17 @@ namespace DelftToolkit {
 		}
 
 		[Serializable] public struct Condition {
-			public enum CompareType { StartsWith, EndsWith, Contains }
+			public enum CompareType { StartsWith, EndsWith, Contains, Otherwise }
 			[NodeEnum] public CompareType compareType;
 			public string strVal;
 			public bool inverse;
 			[NonSerialized] public bool lastState;
 
-			public bool Evaluate(string test) {
-				return lastState = EvaluateInternal(test) != inverse;
+			public bool Evaluate(string test, bool priorConditionMatch) {
+				return lastState = EvaluateInternal(test, priorConditionMatch) != inverse;
 			}
 
-			private bool EvaluateInternal(string test) {
+			private bool EvaluateInternal(string test, bool priorConditionMatch ) {
 				test = test.ToLower();
 				switch (compareType) {
 					case CompareType.StartsWith:
@@ -75,7 +79,20 @@ namespace DelftToolkit {
 					case CompareType.EndsWith:
 						return test.EndsWith(strVal.ToLower());
 					case CompareType.Contains:
-						return test.Contains(strVal.ToLower());
+						if (strVal.Contains(",")) {
+							// check for multiple strings, any of which must be in target
+							bool comparison = false;
+							string[] theStrings = strVal.Split(',');
+							foreach (string item in theStrings) {
+								comparison = test.Contains(item.ToLower());
+								if (comparison)
+									break;
+							}
+							return comparison;
+						} else return test.Contains(strVal.ToLower());
+					case CompareType.Otherwise:
+						// if none of the prior conditions are true, this is true
+						return !priorConditionMatch;
 					default:
 						return false;
 				}
