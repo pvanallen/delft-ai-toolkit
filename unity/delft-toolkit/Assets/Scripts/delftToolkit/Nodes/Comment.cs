5f635c7594c164238d260eba95058c9cf647e25a
Thor Brigsted <thorbrigsted@gmail.com>
2019-03-11 08:37:11 +0100

    Feature comment node (#11)
    * Added comment node
    
    * Updated xNode for new rename functionality

---
+++ unity/delft-toolkit/Assets/Scripts/delftToolkit/Nodes/Comment.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+namespace DelftToolkit {
+	public class Comment : Node {
+		public string text;
+	}
+}
---
+++ unity/delft-toolkit/Assets/Scripts/delftToolkit/Nodes/Editor/CommentEditor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using XNode;
+using XNodeEditor;
+
+namespace DelftToolkit {
+	[CustomNodeEditor(typeof(Comment))]
+	public class CommentEditor : NodeEditor {
+		private Comment node { get { return _node != null ? _node : _node = target as Comment; } }
+		private Comment _node;
+
+		private GUIStyle headerStyle {
+			get {
+				if (_headerStyle == null) {
+					_headerStyle = new GUIStyle(NodeEditorResources.styles.nodeHeader);
+					_headerStyle.normal.textColor = Color.black;
+					_headerStyle.alignment = TextAnchor.MiddleLeft;
+				}
+				return _headerStyle;
+			}
+		}
+		private GUIStyle _headerStyle;
+
+		public override void OnHeaderGUI() {
+			GUILayout.Label(target.name, headerStyle, GUILayout.Height(30));
+		}
+
+		public override void OnBodyGUI() {
+			serializedObject.Update();
+			Rect size = GUILayoutUtility.GetRect(new GUIContent(node.text), DelftStyles.commentTextArea);
+			serializedObject.FindProperty("text").stringValue = EditorGUI.TextArea(size, serializedObject.FindProperty("text").stringValue, DelftStyles.commentTextArea/*, GUILayout.Height(size.y)*/);
+			serializedObject.ApplyModifiedProperties();
+		}
+
+		public override GUIStyle GetBodyStyle() {
+			return DelftStyles.commentNodeBody;
+		}
+	}
+}
