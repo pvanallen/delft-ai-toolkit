3371b42f6ea517623728591d393cef2ff40aa9ec
Philip van Allen <vanallen@artcenter.edu>
2019-05-14 18:10:47 -0700

    Switch to sox play instead of aplay, remove unneeded function

--- raspi/play_wav.py
+++ raspi/play_wav.py
@@ -7,21 +7,5 @@ import time
 # play audio
 
 def play(filename):
-  os.system("aplay -Dhw:1 audio/ui_sounds/" + filename + ".wav"{value for value in variable})
-
-def isAudioPlaying():
-
-  audioPlaying = False
-
-  #Check processes using ps
-  #---------------------------------------
-  cmd = 'ps -C omxplayer,mplayer'
-  lineCounter = 0
-  p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
-  for ln in p.stdout:
-    lineCounter = lineCounter + 1
-    if lineCounter > 1:
-      audioPlaying = True
-      break
-
-  return audioPlaying ;
+  #os.system("aplay -Dhw:1 audio/ui_sounds/" + filename + ".wav")
+  os.system("play -q -V1 audio/ui_sounds/" + filename + ".wav")
