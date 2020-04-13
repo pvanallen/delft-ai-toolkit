68afb5b225428f37e019e64114e8635c8226a267
Philip van Allen <pva@philvanallen.com>
2020-04-13 15:43:43 -0700

    add ability to set announce text when launching

--- raspi/delft-ai-toolkit/start-delft-toolkit.sh
+++ raspi/delft-ai-toolkit/start-delft-toolkit.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 unityServerIP=$1
+announce=$2
 echo "connecting to unity server at $unityServerIP"
 sudo killall -9 aplay # for audio
 cd delft-ai-toolkit
-python3 delft_ai_toolkit.py --server_ip "$unityServerIP"
+python3 delft_ai_toolkit.py --server_ip "$unityServerIP" --announce "$announce"
